6ec7ca814910f5d594ee4359214e3e6b6f75cc5d
Milad Imen <mimen@ucsd.edu>
2016-04-27 18:51:35 -0700

    added page routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/home/');
 });
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+/* GET categories page. */
+router.get('/categories', function(req, res, next) {
+  res.render('categories');
+});
+
 
 module.exports = router;
