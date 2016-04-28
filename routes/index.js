c6b79931a3c1cd2ce9a5c789458aeb79d6f20a4b
Britt Barmeyer <bjbarmeyer@gmail.com>
2016-04-27 19:02:37 -0700

    added all pages to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,14 @@ router.get('/', function(req, res, next) {
   res.redirect('/home/');
 });
 
-/* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
-
 /* GET signup page. */
 router.get('/signup', function(req, res, next) {
   res.render('signup');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
 
 /* GET home page. */
@@ -26,5 +26,29 @@ router.get('/categories', function(req, res, next) {
   res.render('categories');
 });
 
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('products');
+});
+
+/* GET products_browsing page. */
+router.get('/products_browsing', function(req, res, next) {
+  res.render('products_browsing');
+});
+
+/* GET product_order page. */
+router.get('/product_order', function(req, res, next) {
+  res.render('product_order');
+});
+
+/* GET buy_shopping_cart page. */
+router.get('/buy_shopping_cart', function(req, res, next) {
+  res.render('buy_shopping_cart');
+});
+
+/* GET confirmation page. */
+router.get('/confirmation', function(req, res, next) {
+  res.render('confirmation');
+});
 
 module.exports = router;
