1089dff2a2ec06d7283810f0fd3a483088512936
Milad Imen <mimen@ucsd.edu>
2016-05-02 19:29:48 -0700

    started angular code for categories page

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,1 +1,50 @@
-console.log("it works");
+
+var app = angular.module("Categories", []);
+
+app.factory("httpLoader", ["$http", function ($http) {
+    function load(href, done) {
+        $http.get(href)
+            .success(function(data, status, headers, config){
+                done(null, data);
+            })
+            .error(function(data, status, headers, config){
+                console.log(status);
+                var err = new Error(data);
+                err.code = status;
+                done(err);
+            });
+    }
+    return {
+        load: load
+    };
+}]);
+
+
+
+// Library directive - html Element
+app.directive("library", [function (){
+    return {
+        restrict: 'E',
+        templateUrl: 'http://localhost:3000/views/library.html',
+        controller: ["httpLoader", '$http', function (httpLoader, $q, $http, $timeout) {
+            var ctlr = this;
+
+            // Populate the list of songs
+            ctlr.loadCategories = function(){
+                httpLoader.load('http://localhost:3000/api/categories', function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("loaded");
+                        ctlr.categories = result;
+                    }
+                });
+            }
+
+            ctlr.loadCategories();
+
+        }],
+        controllerAs: "ctlr"
+    };
+}]);
