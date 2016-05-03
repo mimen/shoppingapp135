157cf01519bbc5dfb9bdba5f7db25a0d74a4535d
Milad Imen <mimen@ucsd.edu>
2016-05-02 18:37:36 -0700

    removing whitespace using trim()

--- routes/index.js
+++ routes/index.js
@@ -22,13 +22,14 @@ router.get('/signup', verifyLoggedOut, function(req, res, next) {
 router.get('/signup/submit/', verifyLoggedOut, function(req, res, next){
   db.addUser(req.query.username, req.query.type, req.query.age, req.query.state, function(success){
     if (success){
-      db.getUser(req.query.username, function(user, success){
-          if (success){
-            console.log(user);
-            req.session.user = user;
-            res.render('success', {username:username});
-          }
-      });
+      var user = {
+        username: req.query.username,
+        type: req.query.type,
+        age: req.query.age,
+        state: req.query.state
+      }
+      req.session.user = user;
+      res.render('success', {username:req.query.username});
     }
     else
       res.render('failure');
@@ -54,9 +55,8 @@ router.get('/login/submit/', function(req, res, next){
 
 /* GET home page. */
 router.get('/home', verifyLoggedIn, function(req, res, next) {
-  console.log(req.session.user);
   var username = req.session.user.username;
-  var isOwner = req.session.user.type == "owner";
+  var isOwner = req.session.user.type.trim() == "owner";
   res.render('home', {isOwner:isOwner, username:username});
 });
 
@@ -76,9 +76,8 @@ router.get('/categories', verifyLoggedIn, function(req, res, next) {
 
 /* GET products page. */
 router.get('/products', verifyLoggedIn, function(req, res, next) {
-  console.log(req.session.user);
   var username = req.session.user.username;
-  var isOwner = req.session.user.type == "owner";
+  var isOwner = req.session.user.type.trim() == "owner";
   res.render('products', {isOwner:isOwner, username:username});
 });
 
