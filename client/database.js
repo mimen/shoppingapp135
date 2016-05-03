00015f3d958c7838292671052a807399092e9456
Johann Sevilla <jsevilla@ucsd.edu>
2016-05-02 17:18:50 -0700

    hella sql shite

--- client/database.js
+++ client/database.js
@@ -18,58 +18,89 @@ var db = pgp(config.postgres_url);
 */
 
 initializeTables = function(){
-	var query = "CREATE TABLE Users(" + 
+	var query1 = "CREATE TABLE Users(" + 
 		"username char(50) PRIMARY KEY," +
 		"type char(8) NOT NULL," +
 		"age int NOT NULL," +
 		"state char(2) NOT NULL" +
 		");";
 
-	db.any(query)
+  var query2 = "CREATE TABLE Product(" + 
+    "productname char(50) PRIMARY KEY," +
+    "categoryname char(50) FOREIGN KEY REFERENCES Category(categoryname)," +
+    "SKU int NOT NULL," +
+    "price int NOT NULL" +
+    ");";
+
+  var query3 = "CREATE TABLE Category(" + 
+    "categoryname char(50) PRIMARY KEY," +
+    "description  char(50) NOT NULL," +
+    "username  char(50) FOREIGN KEY REFERENCES User(username)" +
+    ");";
+
+  var query4 = "CREATE TABLE Order(" + 
+    "orderID int PRIMARY KEY," +
+    "date char(50) NOT NULL," +
+    "username char[50] FOREIGN KEY REFERENCES User(username)," +
+    "orderNumber int NOT NULL" +
+    ");";
+
+  var query5 = "CREATE TABLE OrderItem(" + 
+    "itemID char(50) PRIMARY KEY," +
+    "productname char(50) FOREIGN KEY REFERENCES Product(productname)," +
+    "orderID int FOREIGN KEY REFERENCES ORDER(orderID)," +
+    "quantity int NOT NULL," +
+    "price int NOT NULL" +
+    ");";
+
+	db.any(query1)
     .then(function (data) {
         console.log(data);
     })
     .catch(function (error) {
        console.log(error);
     });
+
+  db.any(query2)
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+       console.log(error);
+    });
+
+  db.any(query3)
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+       console.log(error);
+    });
+
+  db.any(query4)
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+       console.log(error);
+    });
+      
+  db.any(query5)
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+       console.log(error);
+    });
+
 }
 
-/*
-
-  var query = "CREATE TABLE Product(" + 
-    "productID char(50) PRIMARY KEY," +
-    "categoryID char(50) NOT NULL," +
-    "SKU int NOT NULL," +
-    "price int NOT NULL" +
-    ");";
-
-  var query = "CREATE TABLE Category(" + 
-    "categoryID char(50) PRIMARY KEY," +
-    "description  char(50) NOT NULL," +
-    ");";
-
-  var query = "CREATE TABLE Order(" + 
-    "orderID int PRIMARY KEY," +
-    "date char(50) NOT NULL," +
-    "userID char[50] NOT NULL," +
-    "orderNumber int NOT NULL" +
-    ");";
-
-  var query = "CREATE TABLE OrderItem(" + 
-    "itemID char(50) PRIMARY KEY," +
-    "productID char(50) NOT NULL," +
-    "orderID int NOT NULL," +
-    "quantity int NOT NULL," +
-    "price int NOT NULL" +
-    ");";
-
-*/
 
 
 
 addUser = function(name, type, age, state, done){
 	var query = "INSERT INTO Users" +
-				"(username, type, age, state) " + 
+				 "(username, type, age, state) " + 
 				 "VALUES ('"
 				 	+ name + "', '"
 				 	+ type + "', '" 
@@ -87,8 +118,8 @@ addUser = function(name, type, age, state, done){
 }
 
 getUser = function(name, done){
-    var query = "SELECT * FROM Users" +
-        "WHERE username = '"+ name +"';";
+  var query = "SELECT * FROM Users" +
+              "WHERE username = '"+ name +"';";
 
   db.any(query)
     .then(function (data) {
@@ -103,14 +134,51 @@ getUser = function(name, done){
 }
 
 addCategory = function(name, description, owner){
+  var query = "INSERT INTO Category" +
+        "(categoryname, description, username) " + 
+         "VALUES ('"
+          + name + "', '"
+          + description + "', '" 
+          + owner + "');";
+  db.any(query)
+    .then(function (data) {
+      console.log(data);
+        done(true);
+    })
+    .catch(function (error) {
+      console.log(error);
+      done(false);
 
 }
 
-updateCategory = function(name, description, owner){
+updateCategory = function(cur_name, new_name, new_description, owner){
+  var query = "UPDATE Category" +
+        "SET categoryname = '"+ new_name + "', description = '" + new_description + "'"+
+        "WHERE categoryname = '" + cur_name +"';";
+
+  db.any(query)
+    .then(function (data) {
+      console.log(data);
+        done(true);
+    })
+    .catch(function (error) {
+      console.log(error);
+      done(false);
 
 }
 
-deleteCategory = function(category_id){
+deleteCategory = function(category_name){
+  var query = "DELETE FROM Category" +
+        "WHERE categoryname = '" + category_name + "';";
+
+  db.any(query)
+    .then(function (data) {
+      console.log(data);
+        done(true);
+    })
+    .catch(function (error) {
+      console.log(error);
+      done(false);
 
 }
 
