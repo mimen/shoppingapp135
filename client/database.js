6af7174fcbf5e6a08ff47c6339706468e7c58a90
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-05-23 02:29:23 +0100

    Update database.js
    this will do exactly the same. but it is not good coding either. you should place all that into an external SQL file and use [QueryFile](https://github.com/vitaly-t/pg-promise#query-files) to load and execute it in a single request.

--- client/database.js
+++ client/database.js
@@ -17,87 +17,61 @@ var db = pgp(config.postgres_url);
     });
 */
 
-initializeTables = function(){
+initializeTables = function () {
 
-	var query1 = "CREATE TABLE Users(" + 
-		"uid serial PRIMARY KEY," +
-		"username char(50) NOT NULL UNIQUE," +
-		"type char(8) NOT NULL," +
-		"age int NOT NULL CHECK (age > 0)," +
-		"state char(2) NOT NULL" +
-		");";
+    var query1 = "CREATE TABLE Users(" +
+        "uid serial PRIMARY KEY," +
+        "username char(50) NOT NULL UNIQUE," +
+        "type char(8) NOT NULL," +
+        "age int NOT NULL CHECK (age > 0)," +
+        "state char(2) NOT NULL" +
+        ");";
 
-	var query2 = "CREATE TABLE Categories(" + 
-		"cid serial PRIMARY KEY," +
-	    "categoryname char(50) NOT NULL UNIQUE," +
-	    "description char(50) NOT NULL," +
-	    "username char(50) REFERENCES Users(username)" +
-	    ");";
+    var query2 = "CREATE TABLE Categories(" +
+        "cid serial PRIMARY KEY," +
+        "categoryname char(50) NOT NULL UNIQUE," +
+        "description char(50) NOT NULL," +
+        "username char(50) REFERENCES Users(username)" +
+        ");";
 
-	var query3 = "CREATE TABLE Products(" + 
-		"pid serial PRIMARY KEY," +
-	    "productname char(50) NOT NULL UNIQUE," +
-	    "categoryname char(50) REFERENCES Categories(categoryname)," +
-	    "SKU int NOT NULL UNIQUE CHECK (SKU > 0)," +
-	    "price int NOT NULL CHECK (price > 0)" +
-	    ");";
+    var query3 = "CREATE TABLE Products(" +
+        "pid serial PRIMARY KEY," +
+        "productname char(50) NOT NULL UNIQUE," +
+        "categoryname char(50) REFERENCES Categories(categoryname)," +
+        "SKU int NOT NULL UNIQUE CHECK (SKU > 0)," +
+        "price int NOT NULL CHECK (price > 0)" +
+        ");";
 
-	var query4 = "CREATE TABLE Orders(" +
-		"oid serial PRIMARY KEY," +
-		"date char(50) NOT NULL," +
-		"username char(50) REFERENCES Users(username)," +
-    "total int NOT NULL CHECK (total > 0)," + 
-    "ccn char(16) NOT NULL" + 
-		");";
+    var query4 = "CREATE TABLE Orders(" +
+        "oid serial PRIMARY KEY," +
+        "date char(50) NOT NULL," +
+        "username char(50) REFERENCES Users(username)," +
+        "total int NOT NULL CHECK (total > 0)," +
+        "ccn char(16) NOT NULL" +
+        ");";
 
-	var query5 = "CREATE TABLE LineItems(" + 
-    "lid serial PRIMARY KEY," +
-		"productname char(50) REFERENCES Products(productname)," +
-		"oid serial REFERENCES Orders(oid)," +
-		"quantity int NOT NULL CHECK (quantity > 0)," +
-		"price int NOT NULL CHECK (price > 0)" +
-		");";
+    var query5 = "CREATE TABLE LineItems(" +
+        "lid serial PRIMARY KEY," +
+        "productname char(50) REFERENCES Products(productname)," +
+        "oid serial REFERENCES Orders(oid)," +
+        "quantity int NOT NULL CHECK (quantity > 0)," +
+        "price int NOT NULL CHECK (price > 0)" +
+        ");";
 
-	db.any(query1)
-	    .then(function (data) {
-	        console.log(data);
-	    })
-	    .catch(function (error) {
-	       console.log(error);
-	    });
+    var queries = [query1, query2, query3, query4, query5];
 
-  	db.any(query2)
-	    .then(function (data) {
-	        console.log(data);
-	    })
-	    .catch(function (error) {
-	       console.log(error);
-	    });
-
-  	db.any(query3)
-	    .then(function (data) {
-	        console.log(data);
-	    })
-	    .catch(function (error) {
-	       console.log(error);
-	    });
-
-  	db.any(query4)
-	    .then(function (data) {
-	        console.log(data);
-	    })
-	    .catch(function (error) {
-	       console.log(error);
-	    });
-  
-  	db.any(query5)
-	    .then(function (data) {
-	        console.log(data);
-	    })
-	    .catch(function (error) {
-	       console.log(error);
-	    });
-}
+    db.task(function (t) {
+        return t.batch(queries.map(function (q) {
+            return t.none(q);
+        }));
+    })
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
 
 
 addUser = function(name, role, age, state, done){
